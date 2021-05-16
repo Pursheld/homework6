eb98af5dab69ba2103593869bbda9ce0a3e6dad8
Pursheld <stompee@bk.ru>
2021-05-16 19:30:21 +0300

    Добавьте файлы проекта.

---
+++ homework 6.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stack>  
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    stack <int> steck;  
+
+    int i = 0;
+    int x;
+    cout << "Введите длинну массива: " << endl;
+    cin >> x;
+    cout << "Введите "<< x <<" любых целых чисел: " << endl; 
+
+    while (i != x) {
+        int a;
+        cin >> a;
+
+        steck.push(a);  
+        i++;
+    }
+
+    if (steck.empty()) cout << "Стек не пуст";  
+
+    cout << "Верхний элемент стека: " << steck.top() << endl; 
+    cout << " " << endl;
+    cout << "Удаляем Верхний элемент стека " << endl;
+
+    steck.pop();  
+    cout << " " << endl;
+    cout << "Новый верхний элемент: " << steck.top(); 
+                                                             
+}
